5f3c66ae01cb8869330f4b4afbceccfb3fdf505a
Francisco Ortiz <fortiz40.fo@gmail.com>
2019-08-12 22:10:43 -0700

    Create basic layout of the different sections

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,7 @@ function initilizeSmoothScroll() {
 function setupIntersectionObserver() {
 
     const nav = document.querySelector('.navbar');
+    const socialIcons = document.querySelectorAll('.fab');
     const introContent = document.querySelector('#intro');
 
     const options = {
@@ -34,13 +35,23 @@ function setupIntersectionObserver() {
     const introContentObserver = new IntersectionObserver(function(entries, introContentObserver) {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
+                nav.classList.remove('navbar-light')
+                nav.classList.add('navbar-dark')
                 nav.classList.remove('bg-light')
                 nav.classList.add('bg-transparent')
+                socialIcons.forEach(icon => {
+                    icon.classList.add('light');
+                })
                 
             }
             else{
+                nav.classList.add('navbar-light')
+                nav.classList.remove('navbar-dark')
                 nav.classList.remove('bg-transparent')
                 nav.classList.add('bg-light')
+                socialIcons.forEach( icon => {
+                    icon.classList.remove('light')
+                })
                 
             }
         })
